2c76d812ed2488bad2da93d3a0f355bbc89e6962
melty <Yusei.k.0403@gmail.com>
2026-04-23 12:22:46 +0900

    introducing addComp

--- include/EntityManager.h
+++ include/EntityManager.h
@@ -14,20 +14,31 @@ enum class ComponentBit {
 };
 
 
+inline uint64_t operator|=(uint64_t& baseBit, const ComponentBit& addition) {
+	return static_cast<uint64_t>(baseBit |= static_cast<uint64_t>(addition));
+}
+
+
 class EntityManager {
 public:
 
 	void Tick(float dt);
 
 	void CreateEntity();
+	void AddRenderer(int entity);
+	void AddTransform(int entity);
 
 private:
 
-	std::vector<int> entity_to_index;//[entityID] = index
+	//std::vector<int> entity_to_index;//[entityID] = index
 	std::vector<uint64_t> entity_to_mask;//[entityID] = bit
 	std::vector<uint8_t> isAlive;//[entityID] = 0 or 1; 0 = dead
 
 	std::vector<RendererComponent> rendererComps;
 	std::vector<TransformComponent> transforms;
+
+	int nextEntityId = 0;
+
+	void PushEntity(int id);
 };
 
--- src/EntityManager.cpp
+++ src/EntityManager.cpp
@@ -1,1 +1,38 @@
 #include "EntityManager.h"
+
+void EntityManager::CreateEntity() {
+
+	PushEntity(nextEntityId);
+
+	nextEntityId++;
+}
+
+
+void EntityManager::PushEntity(int id) {
+
+	entity_to_mask.push_back(0ULL);
+	isAlive.push_back(1);//alive
+
+
+}
+
+
+void EntityManager::AddRenderer(int entity) {
+	RendererComponent cmp{};
+	cmp.visible = true;
+	
+	if (entity_to_mask.size() > entity) {
+		entity_to_mask[entity] |= ComponentBit::RENDERER;
+	}
+	else {
+		entity_to_mask.resize(entity + 1);
+		entity_to_mask[entity] = static_cast<uint64_t>(ComponentBit::RENDERER);
+	}
+
+	if (rendererComps.size() <= entity) {
+		rendererComps.resize(entity + 1);
+	}
+	
+	rendererComps[entity] = cmp;
+
+}
